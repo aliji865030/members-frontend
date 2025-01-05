2272734a5947f9cdc9f5f14568b895f3ff6c6967
aliji865030 <abbasalichand786@gmail.com>
2025-01-05 09:07:01 +0530

    Fixed home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
-
 import React from 'react'
 import { Link } from 'react-router-dom';
 
 function Home() {
+    const token = localStorage.getItem('token');
   
     return (
       <div>
         <h1>Welcome to the Community Website</h1>
         <nav>
-          <Link to="/register">Register</Link> | <Link to="/login">Login</Link> | <Link to="/members">Members</Link>
+          <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
+          {token && <span> | <Link to="/members">Members</Link></span>}
         </nav>
       </div>
     );
