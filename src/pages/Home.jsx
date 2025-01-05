9ac40e5a3cc3716205777568e6064cc29dd45ff0
aliji865030 <abbasalichand786@gmail.com>
2025-01-05 08:40:51 +0530

    Completed HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
+
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
+function Home() {
+  
+    return (
+      <div>
+        <h1>Welcome to the Community Website</h1>
+        <nav>
+          <Link to="/register">Register</Link> | <Link to="/login">Login</Link> | <Link to="/members">Members</Link>
+        </nav>
+      </div>
+    );
+  }
 export default Home
