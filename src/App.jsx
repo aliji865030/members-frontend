3b2250d5d5b1370e670ddd16c3fd116fd964f47d
aliji865030 <abbasalichand786@gmail.com>
2025-01-09 14:46:32 +0530

    fixed card

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Members from './pages/Members';
 
 function App() {
   return (
-    <div className='w-full h-screen flex justify-center items-center' 
+    <div className='w-full min-h-screen flex justify-center items-center bg-cover bg-fixed' 
     style={{backgroundImage: "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)"}}
     >
       <Router>
--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { format } from 'date-fns';
 
 function Members() {
   const [members, setMembers] = useState([]);
@@ -36,27 +37,31 @@ function Members() {
 
   return (
     <div
-      style={{
-        backgroundImage: "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)",
-      }}
-      className="px-5 sm:px-10 py-10 sm:py-14 rounded-lg border-2 border-gray-700"
+      className=" w-2/3 px-5 sm:px-10 py-10 sm:py-14 rounded-lg "
     >
       <h2 className="text-5xl font-bold italic text-center">
         Members Directory
       </h2>
-      <ul className=" pt-10 pl-10">
+      <ul className=" pt-10 pl-10 flex flex-col gap-5">
         {members.map((member) => (
-          <li key={member._id} className="text-xl font-bold pt-1 text-gray-800">
-            {member.name} - {member.email}
+          <li key={member._id} className="text-xl font-bold text-gray-800 flex justify-around items-center py-5 rounded-xl border-2 border-gray-500 shadow-xl"
+          style={{
+            backgroundImage: "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)",
+          }}>
+            <span>{member.name.toUpperCase()}</span> 
+            <span>{member.email}</span>
+            <span>{format(new Date(member.joinedDate),'dd/MM/yyyy')}</span>
           </li>
         ))}
       </ul>
+      <div className="flex justify-center items-center">
       <button
         onClick={handleLogout}
-        className="bg-blue-600 text-white px-10 py-2 rounded-md hover:bg-blue-700 mt-10 ml-10"
+        className="bg-blue-600 text-white px-10 py-3 rounded-full hover:bg-blue-700 mt-10 ml-10 w-8/12"
       >
         Logout
       </button>
+      </div>
     </div>
   );
 }
